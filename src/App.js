b17285c73033482032ba4dbbf6a6d205a1d3b7be
ericagong <48196721+ericagong@users.noreply.github.com>
2022-03-19 12:25:52 +0900

    TodoList Web with add/delete/count function

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react'
 
 function App() {
+  const [todo, setTodo] = useState('')
+  const [todos, setTodos] = useState([])
+  const onChange = (event) => {
+    setTodo(event.target.value || '') // For prevent React error.
+  }
+  const onSubmit = (event) => {
+    event.preventDefault() // Prevent <form/>'s original action.
+    if (todo === '') {
+      return
+    }
+    setTodos((current) => [...current, todo])
+    setTodo('')
+  }
+  const onClick = (index) => {
+    const newTodos = todos.filter((td, tdIndex) => index !== tdIndex)
+    setTodos(newTodos)
+  }
+  const printArr = () => {
+    console.log(todos)
+  }
+  useEffect(printArr, [todos])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My Todos ({todos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          text="text"
+          value={todo}
+          placeholder="Write your todo!"
+          onChange={onChange}
+        />
+        <button>Add Todo</button>
+      </form>
+      <hr />
+      {todos.map((td, index) => (
+        <li key={index}>
+          {td}
+          <button onClick={() => onClick(index)}>Delete</button>
+        </li>
+      ))}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
